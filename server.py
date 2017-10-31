04461be53f2ab1539eed0c28a28a4a8051ff96eb
jack-ylt <yueliangtu163@163.com>
2017-10-31 18:57:39 +0800

    Hello world version

---
+++ server.py
@@ -0,0 +1,30 @@
+import http.server
+
+#-------------------------------------------------------------------------------
+
+class RequestHandler(http.server.BaseHTTPRequestHandler):
+    '''Handle HTTP requests by returning a fixed 'page'.'''
+
+    # Page to send back.
+    Page = '''\
+<html>
+<body>
+<p>Hello, web!</p>
+</body>
+</html>
+'''
+
+    # Handle a GET request.
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.send_header("Content-Length", str(len(self.Page)))
+        self.end_headers()
+        self.wfile.write(self.Page.encode())
+
+#-------------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    serverAddress = ('', 8081)
+    server = http.server.HTTPServer(serverAddress, RequestHandler)
+    server.serve_forever()
